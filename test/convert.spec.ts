d1b4a95cc439c1d92576838a3b28db9075e93412
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-11-20 02:09:31 +0000

    test: convert.spec: test merging converted markdown

--- test/convert.spec.ts
+++ test/convert.spec.ts
@@ -10,20 +10,30 @@ const fixturesPath = resolve(projectRoot, 'test/fixtures')
 const tests = readdirSync(fixturesPath)
   .map(path => Path(resolve(fixturesPath, path)))
   .filter(path => path.extension === 'epub')
+  .flatMap(inputPath => [
+    { inputPath, shouldMerge: true },
+    { inputPath, shouldMerge: false },
+  ])
 
 describe(`convert`, () => {
-  for (const inputPath of tests)
-    test(inputPath.fileStem, async () => {
-      const markdown = new Converter(inputPath.fullPath).files
+  for (const { inputPath, shouldMerge } of tests) {
+    const testName = inputPath.fileStem + (shouldMerge ? ' merge' : '')
+    test(testName, async () => {
+      const converter = new Converter(inputPath.fullPath, { shouldMerge })
+      const files = shouldMerge ? converter.mergeProgress! : converter.files
+      const markdown = files
         .toArray()
+        .filter(x => x.content !== undefined)
         .map(({ outputPath, content }) => ({
           outputPath: getRelativePath(inputPath, outputPath),
           content: outputPath.endsWith('md') ? content.toString() : '<image>',
         }))
 
-      const snapshotPath = resolve(projectRoot, 'test/snapshots/unit/convert', inputPath.fileStem)
+      const convertOrMerge = shouldMerge ? 'merge' : 'convert'
+      const snapshotPath = resolve(projectRoot, 'test/snapshots/unit', convertOrMerge, inputPath.fileStem)
       await expect(markdown).toMatchFileSnapshot(snapshotPath)
     })
+  }
 })
 
 const getRelativePath = (inputPath: Path, outputPath: string) =>
