da5267ff99ce0018dc3fcd92fdb6e462c89b4e76
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-12-16 12:33:57 +0000

    test: add snapshot test for convert

---
+++ test/convert.spec.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'node:path'
+import { readdirSync } from 'node:fs'
+import { describe, expect, test } from 'vitest'
+import { Path } from '../src/utils'
+import { projectRoot } from './utilities'
+import { Converter } from '../src/bin/convert'
+
+
+const fixturesPath = resolve(projectRoot, 'test/fixtures')
+const tests = readdirSync(fixturesPath)
+  .map(path => Path(resolve(fixturesPath, path)))
+  .filter(path => path.extension === 'epub')
+
+describe(`convert`, () => {
+  for (const inputPath of tests)
+    test(inputPath.fileStem, async () => {
+      const markdown = new Converter(inputPath.fullPath).quietGenerateFiles()
+        .toArray()
+        .map(({ outputPath, content }) => ({
+          outputPath: getRelativePath(inputPath, outputPath),
+          content: outputPath.endsWith('md') ? content.toString() : '<image>',
+        }))
+
+      const snapshotPath = resolve(projectRoot, 'test/snapshots/unit/convert', inputPath.fileStem)
+      await expect(markdown).toMatchFileSnapshot(snapshotPath)
+    })
+})
+
+const getRelativePath = (inputPath: Path, outputPath: string) =>
+  outputPath.slice(inputPath.directory.length)
