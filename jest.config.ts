142960eb072077346469e4b3eeb4081c803370aa
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-12-16 11:05:18 +0000

    test: switch from jest to vitest
    allows importing esm modules (e.g. package-directory)
    shows more actionable error messages
    less liable to blowing up
    handles typescript without an external helper

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'jest';
-
-const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'js', 'mjs']
-};
-
-export default config;
